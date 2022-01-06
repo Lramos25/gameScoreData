575acf105f0e27219674d7b1c2ebdedaf8a595b9
Lramos25 <luis.ramos_79@yahoo.com>
2022-01-06 15:58:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import re
 import pandas as pd
 import sqlite3
 
-conn = sqlite3.connect('gamedata')
+conn = sqlite3.connect('gamedata2')
+cur = conn.cursor()
 
+# uncomment to create db table but then recomment out
+#cur.execute('''CREATE TABLE gameData2(name TEXT, platform TEXT, releaseDate TEXT, criticScore TEXT, userScore TEXT, developer TEXT, genres TEXT) ''')
 
 def getGameData(url):
-    user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'}
+    user_agent = {'User-agent': 'Mozilla/5.0 '} # adjust to your computer user agent
     response = requests.get(url, headers = user_agent)
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -16,10 +19,9 @@ def getGameData(url):
         name = game.find('h3').text.strip()
 
         platform = game.find('span', class_='data').text.strip()
-        #platform = platform.replace('\n','')
-        #platform = platform.replace(' ','')
         
         releaseDate = game.select('div.clamp-details span')[2].text
+        
         # critic score - data can be in different classes depending on score
         score_list = [
         game.find('div', class_='metascore_w large game positive'),
@@ -40,43 +42,44 @@ def getGameData(url):
 
         userScore = [s.text for s in score_list if s is not None][0]
          
-        # Into the game page
-        # Getting the url of the reviews page:
+        # Retrieve url for reviews page:
         url_info = game.find('a', class_='title')['href']
         url_info = 'https://www.metacritic.com'+url_info
         
-        # Getting into the game page:
         response_info = requests.get(url_info, headers = user_agent)
         soup_info = BeautifulSoup(response_info.text, 'html.parser')
 
-        # Get developer info
+        # Developer
         developer = soup_info.find('li', class_ = 'summary_detail developer')
         
         if developer is not None:
-            developer = developer.find('span',class_='data').text.strip()
-            #developer = developer.replace('\n','')
-            #developer = developer.replace(' ','')  
+            developer = developer.find('span',class_='data').text.strip() 
         else:
             developer = 'No info'  
 
-        # Get genre info (multiple genres are separated in our entry)
+        # Genre info
         genres = soup_info.find('li', class_ = 'summary_detail product_genre')
         
         if genres is not None:
-            genres = genres.find_all('span', class_='data')
-            genre=''
-
-            for item in genres:
-                if genre:
-                    genre = genre + ' / ' + item.text
-                else:
-                    genre = item.text
+            genres = genres.find('span', class_='data').text.strip()
         else:
             genres = 'No info'
 
-        print(name)
+        #print(name, platform, releaseDate, criticScore, userScore, developer, genres)
+        cur.execute('''INSERT INTO gameData2 VALUES(?,?,?,?,?,?,?)''', (name, platform, releaseDate, criticScore, userScore, developer, genres))
     return
 
 #adjust for number of pages up to 180 
 for page in range(1):
-    getGameData('https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?page='+str(page))
+    try:
+        getGameData('https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?page='+str(page))
+    except:
+        pass
+
+conn.commit()
+
+# Read db into dataframe
+df = pd.read_sql_query("SELECT * FROM gameData2", conn)
+print(df)
+
+conn.close()
